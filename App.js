cf9e178af7c15e94389e4ede022419aaa7eea039
Arshutosh <u6378917@anu.edu.au>
2020-09-14 00:27:38 +1000

    Fixed annoying bug preventing full horizontal scroll

--- App.js
+++ App.js
@@ -80,22 +80,20 @@ const App = () => {
   )
 }
 const homescreen = () =>{
-  let dimensions = Dimensions.get("window");
-  let screenwidth = dimensions.width + 200
-  let newsbox = 250
+  let newsbox = 200
 return(
-  <ScrollView style={{backgroundColor: "#F6EEF0"}}>
+  <ScrollView style={{backgroundColor: "#ff6622"}}>
     <View>
       <Text style={{fontWeight :'bold', fontSize: 22, padding: 30}}>
         Happening in Australia
       </Text>
-      <View style = {{height: newsbox, marginLeft: 10}}>
-        <ScrollView horizontal = {true} style = {{paddingHorizontal:20}} showsHorizontalScrollIndicator={false}>
+      <View style = {{height: newsbox, flex:1}}>
+        <ScrollView horizontal = {true} contentContainerStyle={{ paddingLeft:20, flexGrow: 1}}  showsHorizontalScrollIndicator={false}>
           <Newscategories imagesource={{ uri: 'https://imageresizer.static9.net.au/iL1wz7t6ymcD86bayzOWbLTiFp8=/800x0/https%3A%2F%2Fprod.static9.net.au%2Ffs%2F39a838f6-f17d-4633-aada-fdf48d80a091'}} 
-        newsdescription= "The true size of the ancient shark megalodon revealed" />
+        newsdescription= "The true size of the ancient shark megalodon revealed " />
         <Newscategories imagesource={{ uri: 'https://imageresizer.static9.net.au/iL1wz7t6ymcD86bayzOWbLTiFp8=/800x0/https%3A%2F%2Fprod.static9.net.au%2Ffs%2F39a838f6-f17d-4633-aada-fdf48d80a091'}} 
         newsdescription= "The true size of the ancient shark megalodon revealed" />
-        <Newscategories imagesource={{ uri: 'https://imageresizer.static9.net.au/iL1wz7t6ymcD86bayzOWbLTiFp8=/800x0/https%3A%2F%2Fprod.static9.net.au%2Ffs%2F39a838f6-f17d-4633-aada-fdf48d80a091'}} 
+        <Newscategories imagesource={{ uri: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'}} 
         newsdescription= "The true size of the ancient shark megalodon revealed" />
         <Newscategories imagesource={{ uri: 'https://imageresizer.static9.net.au/iL1wz7t6ymcD86bayzOWbLTiFp8=/800x0/https%3A%2F%2Fprod.static9.net.au%2Ffs%2F39a838f6-f17d-4633-aada-fdf48d80a091'}} 
         newsdescription= "The true size of the ancient shark megalodon revealed" />
@@ -106,13 +104,13 @@ return(
       <Text style={{fontWeight :'bold', fontSize: 22, padding: 30}}>
         Happening Globally
       </Text>
-      <View style = {{height: newsbox, marginLeft: 10}}>
-        <ScrollView horizontal = {true} style = {{paddingHorizontal:20}} showsHorizontalScrollIndicator={false}>
+      <View style = {{height: newsbox, flex:1}}>
+        <ScrollView horizontal = {true} contentContainerStyle={{ paddingLeft:20, flexGrow: 1}}  showsHorizontalScrollIndicator={false}>
           <Newscategories imagesource={{ uri: 'https://imageresizer.static9.net.au/iL1wz7t6ymcD86bayzOWbLTiFp8=/800x0/https%3A%2F%2Fprod.static9.net.au%2Ffs%2F39a838f6-f17d-4633-aada-fdf48d80a091'}} 
-        newsdescription= "The true size of the ancient shark megalodon revealed" />
+        newsdescription= "The true size of the ancient shark megalodon revealed " />
         <Newscategories imagesource={{ uri: 'https://imageresizer.static9.net.au/iL1wz7t6ymcD86bayzOWbLTiFp8=/800x0/https%3A%2F%2Fprod.static9.net.au%2Ffs%2F39a838f6-f17d-4633-aada-fdf48d80a091'}} 
         newsdescription= "The true size of the ancient shark megalodon revealed" />
-        <Newscategories imagesource={{ uri: 'https://imageresizer.static9.net.au/iL1wz7t6ymcD86bayzOWbLTiFp8=/800x0/https%3A%2F%2Fprod.static9.net.au%2Ffs%2F39a838f6-f17d-4633-aada-fdf48d80a091'}} 
+        <Newscategories imagesource={{ uri: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80'}} 
         newsdescription= "The true size of the ancient shark megalodon revealed" />
         <Newscategories imagesource={{ uri: 'https://imageresizer.static9.net.au/iL1wz7t6ymcD86bayzOWbLTiFp8=/800x0/https%3A%2F%2Fprod.static9.net.au%2Ffs%2F39a838f6-f17d-4633-aada-fdf48d80a091'}} 
         newsdescription= "The true size of the ancient shark megalodon revealed" />
@@ -123,10 +121,10 @@ return(
       <Text style={{fontWeight :'bold', fontSize: 22, padding: 30}}>
         For You
       </Text>
-      <View style = {{height: newsbox, marginLeft: 10}}>
-        <ScrollView horizontal = {true} style = {{paddingHorizontal:20}} showsHorizontalScrollIndicator={false}>
+      <View style = {{height: newsbox, flex:1}}>
+        <ScrollView horizontal = {true} contentContainerStyle={{ paddingLeft:20, flexGrow: 1}}  showsHorizontalScrollIndicator={false}>
           <Newscategories imagesource={{ uri: 'https://imageresizer.static9.net.au/iL1wz7t6ymcD86bayzOWbLTiFp8=/800x0/https%3A%2F%2Fprod.static9.net.au%2Ffs%2F39a838f6-f17d-4633-aada-fdf48d80a091'}} 
-        newsdescription= "The true size of the ancient shark megalodon revealed" />
+        newsdescription= "The true size of the ancient shark megalodon revealed " />
         <Newscategories imagesource={{ uri: 'https://imageresizer.static9.net.au/iL1wz7t6ymcD86bayzOWbLTiFp8=/800x0/https%3A%2F%2Fprod.static9.net.au%2Ffs%2F39a838f6-f17d-4633-aada-fdf48d80a091'}} 
         newsdescription= "The true size of the ancient shark megalodon revealed" />
         <Newscategories imagesource={{ uri: 'https://imageresizer.static9.net.au/iL1wz7t6ymcD86bayzOWbLTiFp8=/800x0/https%3A%2F%2Fprod.static9.net.au%2Ffs%2F39a838f6-f17d-4633-aada-fdf48d80a091'}} 
