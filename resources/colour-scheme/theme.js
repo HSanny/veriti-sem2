b763ef130921d42c99947c728aa2b257bee60829
Arshutosh  Paudel <u6378917@anu.edu.au>
2020-10-16 13:57:05 +1100

    Update theme.js

--- resources/colour-scheme/theme.js
+++ resources/colour-scheme/theme.js
@@ -1,4 +1,4 @@
-export const purple = '#4C26B2';
+export const purple = '#35246A';
 export const blue = '#87CEFA';
 export const darkGrey = '#555555';
 export const lightGrey = '#E0E0E0';
@@ -7,18 +7,12 @@ export const limegreen = '#42f44b';
 export const white = '#FFFFFF';
 export const black = '#000000'
 export const orange = '#ff6622';
-export const inputcolor = "#f4e6ff"
 
 export const theme = {
   primaryColor: purple,
   secondaryColour: blue,
   backgroundColor: white,
-<<<<<<< HEAD
-  inputcolor: inputcolor,
-=======
->>>>>>> 54bbc6a0c3ae6e3e9eaa7453f81f49a342667995
   textLight: white,
-  textcream: cream,
   textGrey: darkGrey,
   textDark: black,
 }
